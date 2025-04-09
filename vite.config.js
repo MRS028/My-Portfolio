0674655d75a2d0a8126132dcff3e8d1fa69f08c3
Md. Rifat Sheikh <skrifat483@gmail.com>
2025-04-09 18:30:55 +0600

    outDir build added

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'build',
     chunkSizeWarningLimit: 1500 // ওয়ার্নিংটা যাতে না আসে
   }
 })
